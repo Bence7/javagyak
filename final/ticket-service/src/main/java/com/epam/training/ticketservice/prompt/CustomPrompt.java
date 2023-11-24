5b8a448fdf2f21ec40052f1e4e93ef0bdffe6136
Jenei Bence <jeneib13@gmail.com>
2023-11-24 15:38:06 +0100

    Custom prompt.

---
+++ final/ticket-service/src/main/java/com/epam/training/ticketservice/prompt/CustomPrompt.java
@@ -0,0 +1,13 @@
+package com.epam.training.ticketservice.prompt;
+
+import org.jline.utils.AttributedString;
+import org.springframework.shell.jline.PromptProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomPrompt implements PromptProvider {
+    @Override
+    public AttributedString getPrompt() {
+        return new AttributedString("Ticket service>");
+    }
+}
